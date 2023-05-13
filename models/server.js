a8349e8960bc34ffa52253d0c0ad7ef4024fe1ef
victor toledo <querynetworking@gmail.com>
2023-05-12 22:36:21 -0300

    modificacion de paths en server

--- models/server.js
+++ models/server.js
@@ -14,6 +14,7 @@ class Server {
       categorias: '/api/categorias',
       productos:  '/api/productos',
       usuarios:   '/api/usuarios',
+      uploads:    '/api/uploads',
     }
 
     // Conectar a base de datos
@@ -47,6 +48,7 @@ class Server {
     this.app.use( this.paths.categorias, require("../routes/categorias"));
     this.app.use( this.paths.productos, require("../routes/productos"));
     this.app.use( this.paths.usuarios, require("../routes/usuarios"));
+    this.app.use( this.paths.uploads, require("../routes/uploads"));
   }
 
   listen() {
